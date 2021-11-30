d55ea713d300d60b24e1e0158a4df5e0cddead77
Rajani Busani <rajani@Rajanis-MacBook-Pro.local>
2021-11-30 22:34:31 +0100

    endpoints for authentication, users, posts and categories

---
+++ api/index.js
@@ -0,0 +1,44 @@
+// mongodb+srv://rbusani:<password>@blogs.hcdvw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+const express = require("express");
+const app = express();
+const dotenv= require("dotenv");
+const mongoose = require("mongoose")
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+
+dotenv.config();
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>console.log("connected to MongoDB"))
+.catch((err)=>console.log(err))
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, "req.body.name");
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+  });
+
+
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/categories", categoryRoute);
+
+
+
+app.listen("5000", ()=>{
+    console.log("Server is running")
+})
