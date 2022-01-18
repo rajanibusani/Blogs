4b668696d36801a9d1b15c30b4b88ed60a5cb323
Rajani Busani <rajani@Rajanis-MacBook-Pro.local>
2022-01-18 14:22:53 +0100

    added a post

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -24,6 +24,7 @@ router.post("/register", async(req, res)=>{
 //LOGIN
 router.post("/login", async(req, res)=>{
     try{
+        console.log("login-auth")
         const user = await User.findOne({ username : req.body.username});
         !user && res.status(400).json("Entered wrong credentials")
 
